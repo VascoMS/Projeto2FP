33f264e95397caee1a5d3793051d817aa5e17a3a
VascoMS <73793922+VascoMS@users.noreply.github.com>
2020-12-31 10:22:24 +0000

    Versão com 3 erros mooshak da feira

--- Projeto2.py
+++ Projeto2.py
@@ -1,14 +1,15 @@
 # VASCO SILVA IST199132
+
 def cria_posicao(c, l):
     valores_possiveis_l = ('1', '2', '3')
     valores_possiveis_c = ('a', 'b', 'c')
     if l not in valores_possiveis_l or c not in valores_possiveis_c:
-        raise ValueError('cria posicao: argumentos invalidos')
+        raise ValueError('cria_posicao: argumentos invalidos')
     return [c, l]
 
 
 def cria_copia_posicao(p):
-    return p
+    return p.copy()
 
 
 def obter_pos_c(p):
@@ -57,43 +58,52 @@ def obter_posicoes_adjacentes(p):
     valores_posiveis_l = ('1', '2', '3')
     pos_possiveis = [cria_posicao(c, l) for l in valores_posiveis_l for c in valores_posiveis_c]
     res_validos = ((1, 0), (0, 1), (-1, 0), (0, -1))  # as posicoes adjacentes a uma posicao podem ser obtidas
+    res_validos_diag = ((1, 1), (-1, -1), (1, -1), (-1, 1))
+    valores_com_diag = (cria_posicao('a', '1'), cria_posicao('c', '1'), cria_posicao('a', '3'), cria_posicao('c', '3'),
+                        cria_posicao('b', '2'))
     # subtraindo o ord da coluna e o valor da linha a posicao argumento, e essa conta tem de originar um dos
-    # reultados deste tuplo
-    return tuple(i for i in pos_possiveis if (ord(obter_pos_c(p)) - ord(obter_pos_c(i)), int(obter_pos_l(p)) -
-                                              int(obter_pos_l(i))) in res_validos)
+    # resultados deste tuplo
+    return tuple(i for i in pos_possiveis if ((ord(obter_pos_c(p)) - ord(obter_pos_c(i)), int(obter_pos_l(p)) -
+                                               int(obter_pos_l(i))) in res_validos) or (i in valores_com_diag and
+                                                                                        (ord(obter_pos_c(p)) - ord(
+                                                                                            obter_pos_c(i)),
+                                                                                         int(obter_pos_l(p)) -
+                                                                                         int(obter_pos_l(
+                                                                                             i))) in res_validos_diag))
 
 
 def cria_peca(peca):
-    pecas = ('X', 'O', ' ')
-    if peca not in pecas:
-        raise ValueError('cria peca: argumento invalido')
-    return peca
+    pecas = {'X': 1, 'O': -1, ' ': 0}
+    if type(peca) != str or peca not in pecas.keys():
+        raise ValueError('cria_peca: argumento invalido')
+    return pecas[peca]
 
 
 def cria_copia_peca(peca):
-    return peca
+    return int(peca)
 
 
 def eh_peca(arg):
-    pecas = ('X', 'O', ' ')
-    return type(arg) == str and arg in pecas
+    pecas = (1, -1, 0)
+    return type(arg) == int and arg in pecas
 
 
 def pecas_iguais(p1, p2):
-    return p1 == p2
+    return p1 == p2 and type(p1) == int and type(p2) == int
 
 
 def peca_para_str(peca):
-    return '[{}]'.format(peca)
+    dic = {1: 'X', -1: 'O', 0: ' '}
+    return '[{}]'.format(dic[peca])
 
 
 def peca_para_inteiro(peca):
     return 1 if peca_para_str(peca) == '[X]' else (-1 if peca_para_str(peca) == '[O]' else 0)
 
 
-def inteiro_para_peca(peca):
-    dic_pecas = {1: 'X', -1: 'O', 0: ' '}
-    return dic_pecas[peca]
+def inteiro_para_peca(inteiro):
+    dic_pecas = {1: cria_peca('X'), -1: cria_peca('O'), 0: cria_peca(' ')}
+    return dic_pecas[inteiro]
 
 
 def cria_tabuleiro():
@@ -101,37 +111,33 @@ def cria_tabuleiro():
 
 
 def cria_copia_tabuleiro(tab):
-    return tab
+    return [linha.copy() for linha in tab]
 
 
 def obter_peca(tab, pos):
     coordenadas = pos_coordenadas(posicao_para_str(pos))
-    return inteiro_para_peca(tab[coordenadas[1]][coordenadas[0]])
+    return tab[coordenadas[1]][coordenadas[0]]
 
 
 def obter_vetor(tab, conjunto):
     valores_possiveis_l = ('1', '2', '3')
     col = ()
     if conjunto in valores_possiveis_l:
-        return tuple(inteiro_para_peca(peca) for peca in tab[int(conjunto) - 1])
+        return tuple(peca for peca in tab[int(conjunto) - 1])
     for linha in range(len(tab)):
         col += (tab[linha][ord(conjunto) - ord('a')],)
-    return tuple(inteiro_para_peca(peca) for peca in col)
-
-
-def obter_vetor_int(tab, conjunto):
-    return tuple(peca_para_inteiro(peca) for peca in obter_vetor(tab, conjunto))
+    return tuple(peca for peca in col)
 
 
 def coloca_peca(tab, peca, pos):
     coordenadas = pos_coordenadas(posicao_para_str(cria_posicao(obter_pos_c(pos), obter_pos_l(pos))))
-    tab[coordenadas[1]][coordenadas[0]] = peca_para_inteiro(cria_peca(peca))
+    tab[coordenadas[1]][coordenadas[0]] = peca
     return tab
 
 
 def remove_peca(tab, pos):
     coordenadas = pos_coordenadas(posicao_para_str(pos))
-    tab[coordenadas[1]][coordenadas[0]] = peca_para_inteiro(cria_peca(' '))
+    tab[coordenadas[1]][coordenadas[0]] = cria_peca(' ')
     return tab
 
 
@@ -141,9 +147,20 @@ def move_peca(tab, p1, p2):
 
 
 def jogadores_ganhadores(tab):
-    dic_ganhador = {-1: (-1, -1, -1), 1: (1, 1, 1)}
-    return [key for key in dic_ganhador for i in range(1, 4) if obter_vetor_int(tab, str(i)) == dic_ganhador[key]
-            or obter_vetor_int(tab, chr(ord('a') + (i - 1))) == dic_ganhador[key]]
+    dic_ganhador = {-1: (cria_peca('O'), cria_peca('O'), cria_peca('O')),
+                    1: (cria_peca('X'), cria_peca('X'), cria_peca('X'))}
+    return [key for key in dic_ganhador for i in range(1, 4) if obter_vetor(tab, str(i)) == dic_ganhador[key]
+            or obter_vetor(tab, chr(ord('a') + (i - 1))) == dic_ganhador[key]]
+
+
+def n_pecas_jog(tab, jog):
+    valores_posiveis_c = ('a', 'b', 'c')
+    valores_posiveis_l = ('1', '2', '3')
+    pos_possiveis = [cria_posicao(c, l) for l in valores_posiveis_l for c in valores_posiveis_c]
+    res = 0
+    for i in pos_possiveis:
+        res += 1 if (pecas_iguais(obter_peca(tab, i), jog)) else 0
+    return res
 
 
 def eh_tabuleiro(tab):
@@ -160,40 +177,32 @@ def eh_tabuleiro(tab):
     False: Se o tab nao respeitar alguma das condicoes referidas acima,
            nao constituindo um tabuleiro valido.
     """
-    valores_posiveis = (-1, 0, 1)
-    n_pecas = [len([j for i in tab for j in i if j == 1]) - len([j for i in tab for j in i if j == -1]) < 1]
+    valores_posiveis = (cria_peca('O'), cria_peca('X'), cria_peca(' '))
+    n_pecas = [n_pecas_jog(tab, jog) for jog in valores_posiveis[:2]]
+    dif_pecas = [abs(len([j for i in tab for j in i if j == cria_peca('X')]) - len(
+        [j for i in tab for j in i if j == cria_peca('O')])) <= 1]
     res = [type(tab) == list and len(tab) == 3] + [
-        type(linha) == list and len(linha) == 3 and type(valor) == int and valor
-        in valores_posiveis for linha in tab for valor in linha] + [len(jogadores_ganhadores(tab)) < 1] + n_pecas
+        type(linha) == list and len(linha) == 3 and type(valor) == int and valor in valores_posiveis for linha in tab
+        for valor in linha] + [len(jogadores_ganhadores(tab)) <= 1] + dif_pecas + [i <= 3 for i in n_pecas]
     return False not in res
 
 
 def eh_posicao_livre(tab, pos):
-    return peca_para_inteiro(obter_peca(tab, pos)) == peca_para_inteiro(cria_peca(' '))
+    return pecas_iguais(obter_peca(tab, pos), cria_peca(' '))
 
 
 def tabuleiros_iguais(t1, t2):
-    return t1 == t2
-
-
-def n_pecas_jog(tab, jog):
-    valores_posiveis_c = ('a', 'b', 'c')
-    valores_posiveis_l = ('1', '2', '3')
-    pos_possiveis = [cria_posicao(c, l) for l in valores_posiveis_l for c in valores_posiveis_c]
-    res = 0
-    for i in pos_possiveis:
-        res += 1 if obter_peca(tab, i) == cria_peca(jog) else 0
-    return res
+    return t1 == t2 and eh_tabuleiro(t1) and eh_tabuleiro(t2)
 
 
 def tabuleiro_para_str(tab):
-    return '   a   b   c\n1 [{}]-[{}]-[{}]\n   | \\ | / |\n2 [{}]-[{}]-[{}]\n   | / | \\ |\n3 [{}]-[{}]-[{}]'.format(
-        obter_peca(tab, cria_posicao('a', '1')),
-        obter_peca(tab, cria_posicao('b', '1')), obter_peca(tab, cria_posicao('c', '1')),
-        obter_peca(tab, cria_posicao('a', '2')), obter_peca(tab, cria_posicao('b', '2')),
-        obter_peca(tab, cria_posicao('c', '2')),
-        obter_peca(tab, cria_posicao('a', '3')), obter_peca(tab, cria_posicao('b', '3')),
-        obter_peca(tab, cria_posicao('c', '3')))
+    return '   a   b   c\n1 {}-{}-{}\n   | \\ | / |\n2 {}-{}-{}\n   | / | \\ |\n3 {}-{}-{}'.format(
+        peca_para_str(obter_peca(tab, cria_posicao('a', '1'))),
+        peca_para_str(obter_peca(tab, cria_posicao('b', '1'))), peca_para_str(obter_peca(tab, cria_posicao('c', '1'))),
+        peca_para_str(obter_peca(tab, cria_posicao('a', '2'))), peca_para_str(obter_peca(tab, cria_posicao('b', '2'))),
+        peca_para_str(obter_peca(tab, cria_posicao('c', '2'))),
+        peca_para_str(obter_peca(tab, cria_posicao('a', '3'))), peca_para_str(obter_peca(tab, cria_posicao('b', '3'))),
+        peca_para_str(obter_peca(tab, cria_posicao('c', '3'))))
 
 
 def tuplo_para_tabuleiro(tup):
@@ -201,10 +210,11 @@ def tuplo_para_tabuleiro(tup):
 
 
 def obter_ganhador(tab):
-    dic_ganhador = {-1: (-1, -1, -1), 1: (1, 1, 1)}
-    res = [key for key in dic_ganhador for i in range(1, 4) if obter_vetor_int(tab, str(i)) == dic_ganhador[key]
-           or obter_vetor_int(tab, chr(ord('a') + (i - 1))) == dic_ganhador[key]]
-    return inteiro_para_peca(res[0]) if res else cria_peca(' ')
+    dic_ganhador = {cria_peca('O'): (cria_peca('O'), cria_peca('O'), cria_peca('O')),
+                    cria_peca('X'): (cria_peca('X'), cria_peca('X'), cria_peca('X'))}
+    res = [key for key in dic_ganhador for i in range(1, 4) if obter_vetor(tab, str(i)) == dic_ganhador[key]
+           or obter_vetor(tab, chr(ord('a') + (i - 1))) == dic_ganhador[key]]
+    return res[0] if res else cria_peca(' ')
 
 
 def obter_posicoes_livres(tab):
@@ -218,10 +228,14 @@ def obter_posicoes_jogador(tab, jog):
     valores_posiveis_c = ('a', 'b', 'c')
     valores_posiveis_l = ('1', '2', '3')
     return tuple(cria_posicao(c, l) for l in valores_posiveis_l for c in valores_posiveis_c if
-                 peca_para_inteiro(obter_peca(tab, cria_posicao(c, l))) == peca_para_inteiro(cria_peca(jog)))
+                 pecas_iguais(obter_peca(tab, cria_posicao(c, l)), jog))
 
 
 #  2.2 Funcoes adicionais
+def block(tab, jog):
+    return not [pos_adj for pos in obter_posicoes_jogador(tab, jog) for pos_adj in obter_posicoes_adjacentes(pos)
+                if eh_posicao_livre(tab, pos_adj)] and n_pecas_jog(tab, jog) != 0
+
 
 def obter_movimento_manual(tab, jog):
     valores_posiveis_c = ('a', 'b', 'c')
@@ -230,29 +244,33 @@ def obter_movimento_manual(tab, jog):
     if n_pecas_jog(tab, jog) < 3:
         pos = input('Turno do jogador. Escolha uma posicao: ')
         if pos not in pos_possiveis or not eh_posicao_livre(tab, pos):
-            raise ValueError('obter movimento manual: escolha invalida')
+            raise ValueError('obter_movimento_manual: escolha invalida')
         return cria_posicao(obter_pos_c(pos), obter_pos_l(pos)),
     mov = input('Turno do jogador. Escolha um movimento: ')
-    if (obter_peca(tab, cria_posicao(obter_pos_c(mov[:2]), obter_pos_l(mov[:2]))) != cria_peca(jog) or
-        obter_peca(tab, cria_posicao(obter_pos_c(mov[2:]), obter_pos_l(mov[2:]))) not in (
-                cria_peca(' '), cria_peca(jog))) or mov[:2] not in pos_possiveis or mov[2:] not in pos_possiveis:
-        raise ValueError('obter movimento manual: escolha invalida')
+    if mov[:2] not in pos_possiveis or mov[2:] not in pos_possiveis or \
+            (obter_peca(tab, cria_posicao(mov[:2][0], mov[:2][1])) != jog or
+             obter_peca(tab, cria_posicao(mov[2:][0], mov[2:][1])) not in (
+                     cria_peca(' '), jog)) or cria_posicao(mov[2:][0], mov[2:][1]) not in obter_posicoes_adjacentes(
+        cria_posicao(mov[:2][0], mov[:2][1])) + (cria_posicao(mov[:2][0], mov[:2][1]),) or (
+            cria_posicao(mov[2:][0], mov[2:][1]) == cria_posicao(mov[:2][0], mov[:2][1]) and not block(tab, jog)):
+        raise ValueError('obter_movimento_manual: escolha invalida')
     return cria_posicao(obter_pos_c(mov[:2]), obter_pos_l(mov[:2])), cria_posicao(obter_pos_c(mov[2:]),
                                                                                   obter_pos_l(mov[2:]))
 
 
 def auto_coloc(tab, jog):
-    pos_livres = [posicao_para_str(pos) for pos in obter_posicoes_livres(tab)]
+    pos_livres = [pos for pos in obter_posicoes_livres(tab)]
     adv = inteiro_para_peca(-peca_para_inteiro(jog))
-    centro = posicao_para_str(cria_posicao('b', '2'))
-    cantos = {'Canto1': posicao_para_str(cria_posicao('a', '1')), 'Canto2': posicao_para_str(cria_posicao('c', '1')),
-              'Canto3': posicao_para_str(cria_posicao('a', '3')), 'Canto4': posicao_para_str(cria_posicao('c', '3'))}
-    laterais = {'lat1': posicao_para_str(cria_posicao('b', '1')), 'lat2': posicao_para_str(cria_posicao('a', '2')),
-                'lat3': posicao_para_str(cria_posicao('c', '2')), 'lat4': posicao_para_str(cria_posicao('b', '3'))}
+    centro = cria_posicao('b', '2')
+    cantos = {'Canto1': cria_posicao('a', '1'), 'Canto2': cria_posicao('c', '1'),
+              'Canto3': cria_posicao('a', '3'), 'Canto4': cria_posicao('c', '3')}
+    laterais = {'lat1': cria_posicao('b', '1'), 'lat2': cria_posicao('a', '2'),
+                'lat3': cria_posicao('c', '2'), 'lat4': cria_posicao('b', '3')}
     for pos in pos_livres:
-        if obter_ganhador(coloca_peca(tab, cria_peca(jog), pos)) == peca_para_str(cria_peca(jog)):
+        if pecas_iguais(obter_ganhador(coloca_peca(cria_copia_tabuleiro(tab), jog, pos)), jog):
             return pos
-        elif obter_ganhador(coloca_peca(tab, adv, pos)) == peca_para_str(cria_peca(adv)):
+    for pos in pos_livres:
+        if pecas_iguais(obter_ganhador(coloca_peca(cria_copia_tabuleiro(tab), adv, pos)), adv):
             return pos
     if centro in pos_livres:
         return centro
@@ -267,16 +285,17 @@ def auto_coloc(tab, jog):
 def minimax(tab, jog, prof, seq_mov):
     adv = inteiro_para_peca(-peca_para_inteiro(jog))
     melhor_seq_mov = ()
-    if obter_ganhador(tab) != cria_peca(' ') or prof == 0:
+    if not pecas_iguais(obter_ganhador(tab), cria_peca(' ')) or prof == 0:
         return peca_para_inteiro(obter_ganhador(tab)), seq_mov
     else:
-        melhor_res = peca_para_inteiro(adv)
+        melhor_res = adv
         for pos in obter_posicoes_jogador(tab, jog):
             for pos_adj in obter_posicoes_adjacentes(pos):
                 if eh_posicao_livre(tab, pos_adj):
                     novo_tab = move_peca(cria_copia_tabuleiro(tab), pos, pos_adj)
                     novo_res, nova_seq_mov = minimax(novo_tab, adv, prof - 1, seq_mov + ((pos, pos_adj)))
-                    if not melhor_seq_mov or (jog == cria_peca('X') and novo_res > melhor_res) or (jog == cria_peca('O') and novo_res < melhor_res):
+                    if not melhor_seq_mov or (pecas_iguais(jog, cria_peca('X')) and novo_res > melhor_res) or \
+                            (pecas_iguais(jog, cria_peca('O')) and novo_res < melhor_res):
                         melhor_res, melhor_seq_mov = novo_res, nova_seq_mov
         return melhor_res, melhor_seq_mov
 
@@ -284,16 +303,66 @@ def minimax(tab, jog, prof, seq_mov):
 def obter_movimento_auto(tab, jog, dif):
     if n_pecas_jog(tab, jog) < 3:
         return auto_coloc(tab, jog),
-    if dif == 'facil' or minimax(tab, jog, 1, ())[0] == peca_para_inteiro(cria_peca(' ')):
+    if block(tab, jog):
+        return obter_posicoes_jogador(tab, jog)[0], obter_posicoes_jogador(tab, jog)[0]
+    if dif == 'facil' or (dif == 'normal' and minimax(tab, jog, 1, ())[0] == peca_para_inteiro(cria_peca(' '))):
         for pos in obter_posicoes_jogador(tab, jog):
-            if len(obter_posicoes_adjacentes(pos)) > 0:
-                return pos, obter_posicoes_adjacentes(pos)[0]
+            for pos_adj in obter_posicoes_adjacentes(pos):
+                if eh_posicao_livre(tab, pos_adj):
+                    return pos, pos_adj
     if dif == 'normal':
-        print(minimax(tab, jog, 1, ()))
-        return minimax(tab, jog, 1, ())[1][1]
-    if dif == 'perfeito':
-        return minimax(tab, jog, 5, ())[1][1]
+        return minimax(tab, jog, 1, ())[1]
+    if dif == 'dificil':
+        return minimax(tab, jog, 5, ())[1][:2]
 
 
-
-print(obter_ganhador(((1,0,-1),(0,1,-1),(1,-1,0))))
+def moinho(jog, dif):
+    dic_peca = {'[X]': cria_peca('X'), '[O]': cria_peca('O')}
+    niveis = ('facil', 'normal', 'dificil')
+    if type(jog) != str or type(dif) != str or jog not in dic_peca or dif not in niveis:
+        raise ValueError('moinho: argumentos invalidos')
+    tab = cria_tabuleiro()
+    jog = dic_peca[jog]
+    res = cria_peca(' ')
+    adv = inteiro_para_peca(-peca_para_inteiro(jog))
+    print('Bem-vindo ao JOGO DO MOINHO. Nivel de dificuldade {}.'.format(dif))
+    print(tabuleiro_para_str(tab))
+    if pecas_iguais(jog, '[O]'):
+        while pecas_iguais(res, cria_peca(' ')):
+            print('Turno do computador ({}):'.format(dif))
+            if len(obter_posicoes_jogador(tab, adv)) < 3:
+                tab = coloca_peca(tab, adv, obter_movimento_auto(tab, adv, dif)[0])
+            else:
+                mov = obter_movimento_auto(tab, adv, dif)
+                tab = move_peca(tab, mov[0], mov[1])
+            res = obter_ganhador(tab)
+            print(tabuleiro_para_str(tab))
+            if not pecas_iguais(res, cria_peca(' ')):
+                break
+            if len(obter_posicoes_jogador(tab, jog)) < 3:
+                tab = coloca_peca(tab, jog, obter_movimento_manual(tab, jog)[0])
+            else:
+                mov = obter_movimento_manual(tab, jog)
+                tab = move_peca(tab, mov[0], mov[1])
+            res = obter_ganhador(tab)
+            print(tabuleiro_para_str(tab))
+    else:
+        while pecas_iguais(res, cria_peca(' ')):
+            if len(obter_posicoes_jogador(tab, adv)) < 3:
+                tab = coloca_peca(tab, jog, obter_movimento_manual(tab, jog)[0])
+            else:
+                mov = obter_movimento_manual(tab, jog)
+                tab = move_peca(tab, mov[0], mov[1])
+            res = obter_ganhador(tab)
+            print(tabuleiro_para_str(tab))
+            if not pecas_iguais(res, cria_peca(' ')):
+                break
+            print('Turno do computador ({}):'.format(dif))
+            if len(obter_posicoes_jogador(tab, adv)) < 3:
+                tab = coloca_peca(tab, adv, obter_movimento_auto(tab, adv, dif)[0])
+            else:
+                mov = obter_movimento_auto(tab, adv, dif)
+                tab = move_peca(tab, mov[0], mov[1])
+            res = obter_ganhador(tab)
+            print(tabuleiro_para_str(tab))
+    return peca_para_str(res)
