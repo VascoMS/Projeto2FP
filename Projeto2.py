cad2ea567e5893bf889749c39ae19f3ca6ad9fac
VascoMS <73793922+VascoMS@users.noreply.github.com>
2021-01-04 22:32:49 +0000

    Versão com TAD verificadas falta alguns comments

--- Projeto2.py
+++ Projeto2.py
@@ -1,6 +1,14 @@
 # VASCO SILVA IST199132
 
+#  2.1.1 TAD posicao
+
 def cria_posicao(c, l):
+    """ Funcao de operacao basica para criar uma posicao. Foi escolhida a representacao na forma de lista.
+
+    :param c: (str) coluna do tabuleiro representada pelos valores em valores_possiveis_c
+    :param l: (str) linha do tabuleiro representada pelos valores em valores_possiveis_1
+    :return:  (list) lista com o primeiro termo a representar o valor da coluna e o segundo a representar o da linha
+    """
     valores_possiveis_l = ('1', '2', '3')
     valores_possiveis_c = ('a', 'b', 'c')
     if l not in valores_possiveis_l or c not in valores_possiveis_c:
@@ -9,18 +17,39 @@ def cria_posicao(c, l):
 
 
 def cria_copia_posicao(p):
+    """ Funcao de operacao basica para criar uma copia de uma posicao.
+
+    :param p: (list) posicao com a representacao escolhida neste caso uma lista
+    :return: (list) uma nova copia da posicao
+    """
     return p.copy()
 
 
 def obter_pos_c(p):
+    """ Funcao de operacao basica para obter a coluna da posicao passada como argumento.
+
+    :param p: (list) posicao com a representacao escolhida neste caso uma lista
+    :return: (str) coluna da posicao
+    """
     return p[0]
 
 
 def obter_pos_l(p):
+    """ Funcao de operacao basica para obter a linha da posicao passada como argumento.
+
+    :param p: (list) posicao com a representacao escolhida neste caso uma lista
+    :return: (str) linha da posicao
+    """
+
     return p[1]
 
 
 def eh_posicao(arg):
+    """ Funcao de operacao basica para verificar se o argumento introduzido se trata de uma posicao
+
+    :param arg: (any type) argumento de qualquer tipo.
+    :return: (bool) True se for uma posicao valida False caso contrario.
+    """
     valores_possiveis_l = ('1', '2', '3')
     valores_possiveis_c = ('a', 'b', 'c')
     return type(arg) == list and len(arg) == 2 and obter_pos_c(arg) in valores_possiveis_c and obter_pos_l(arg) \
@@ -28,32 +57,31 @@ def eh_posicao(arg):
 
 
 def posicoes_iguais(p1, p2):
+    """ Funcao de operacao basica para verificar se duas posicoes sao iguais
+
+    :param p1: (list) posicao com a representacao escolhida neste caso uma lista
+    :param p2: (list) posicao com a representacao escolhida neste caso uma lista
+    :return: (bool) True caso as posicoes sejam iguais False caso contrario.
+    """
     return eh_posicao(p1) and eh_posicao(p2) and p1 == p2
 
 
 def posicao_para_str(p):
+    """ Funcao de operacao basica para representar uma posicao na forma de string
+
+    :param p: (list) posicao com a representacao escolhida neste caso uma lista
+    :return: (str) posicao na forma de string
+    """
     return obter_pos_c(p) + obter_pos_l(p)
 
 
-def pos_coordenadas(pos):
-    """ Funcao auxiliar de obtencao de coordenadas(index da posicao na linha e index da linha de uma posicao num tuplo)
-        de um posicao especificada
-
-    Parametros:
-
-    pos(int): posicao no tabuleiro.
-
-    Returns:
-
-    tup: coordenadas da posicao especificada.
-
-    """
-    pos_dic = {'a1': (0, 0), 'b1': (1, 0), 'c1': (2, 0), 'a2': (0, 1), 'b2': (1, 1), 'c2': (2, 1), 'a3': (0, 2),
-               'b3': (1, 2), 'c3': (2, 2)}
-    return pos_dic[posicao_para_str(pos)]
-
-
 def obter_posicoes_adjacentes(p):
+    """ Funcao de alto nivel que permite obter as posicoes adjacentes num tabuleiro de jogo do moinho da posicao
+        especificada.
+
+    :param p:  posicao com qualquer representacao
+    :return: (tuple) tuplo com as posicoes adjacentes
+    """
     valores_posiveis_c = ('a', 'b', 'c')
     valores_posiveis_l = ('1', '2', '3')
     pos_possiveis = [cria_posicao(c, l) for l in valores_posiveis_l for c in valores_posiveis_c]
@@ -61,18 +89,25 @@ def obter_posicoes_adjacentes(p):
     res_validos_diag = ((1, 1), (-1, -1), (1, -1), (-1, 1))
     valores_com_diag = (cria_posicao('a', '1'), cria_posicao('c', '1'), cria_posicao('a', '3'), cria_posicao('c', '3'),
                         cria_posicao('b', '2'))
-    # subtraindo o ord da coluna e o valor da linha a posicao argumento, e essa conta tem de originar um dos
-    # resultados deste tuplo
+    # subtraindo o ord da coluna e o valor da linha a posicao argumento, se a posicao for adjacente obtemos um dos
+    # resultados do tuplo res_validos ou res_validos_diag visto que as posicoes adjacentes se encontram um valor para a
+    # direita, para esquerda, para cima ou para baixo ou mesmo uma combinacao de dois membros deste conjunto dependendo
+    # da posicao no tabuleiro
     return tuple(i for i in pos_possiveis if ((ord(obter_pos_c(p)) - ord(obter_pos_c(i)), int(obter_pos_l(p)) -
                                                int(obter_pos_l(i))) in res_validos) or (i in valores_com_diag and
                                                                                         (ord(obter_pos_c(p)) - ord(
                                                                                             obter_pos_c(i)),
-                                                                                         int(obter_pos_l(p)) -
-                                                                                         int(obter_pos_l(
+                                                                                         int(obter_pos_l(p))
+                                                                                         - int(obter_pos_l(
                                                                                              i))) in res_validos_diag))
-
+#  2.1.2 TAD peca
 
 def cria_peca(peca):
+    """ Funcao de operacao basica que cria uma peca de acordo com a representacao escolhida, neste caso um inteiro
+
+    :param peca: (str) recebe um representante de cada peca na forma 'X', 'O' ou ' '
+    :return: (int) devolve um inteiro correspondente, 1 oara 'X', -1 para 'O' e 0 para ' '
+    """
     pecas = {'X': 1, 'O': -1, ' ': 0}
     if type(peca) != str or peca not in pecas.keys():
         raise ValueError('cria_peca: argumento invalido')
@@ -80,39 +115,89 @@ def cria_peca(peca):
 
 
 def cria_copia_peca(peca):
+    """ Funcao de operacao basica que cria uma copia da peca especificada
+
+    :param peca: (int) peca na representacao escolhida neste caso um inteiro
+    :return: (int) copia da peca
+    """
     return int(peca)
 
 
 def eh_peca(arg):
+    """ Funcao de operacao basica que verifica se o argumento introduzido se trata de uma peca
+
+    :param arg: (any type) argumento de qualquer tipo
+    :return: (bool) True se o argumento se tratar de um peca do tipo 1, -1 ou 0 e False caso contrario
+    """
     pecas = (1, -1, 0)
     return type(arg) == int and arg in pecas
 
 
 def pecas_iguais(p1, p2):
-    return p1 == p2 and type(p1) == int and type(p2) == int
+    """ Funcao de operacao basica para verificar se duas pecas ssao iguais
+
+    :param p1: (int) peca na representacao escolhida neste caso um inteiro
+    :param p2: (int) peca na representacao escolhida neste caso um inteiro
+    :return: (bool) True se as duas pecas forem iguais False caso contrario
+    """
+    return p1 == p2
 
 
 def peca_para_str(peca):
+    """ Funcao de operacao basica que transforma um peca na representacaao escolhida numa peca na forma '[X]', '[O]' ou
+        '[ ]'
+
+    :param peca: (int) peca na representacao escolhida neste caso um inteiro
+    :return: peca na forma '[X]', '[O]' ou '[ ]'
+    """
     dic = {1: 'X', -1: 'O', 0: ' '}
     return '[{}]'.format(dic[peca])
 
 
 def peca_para_inteiro(peca):
+    """ Funcao de alto nivel que transforma uma peca na sua representacao sob a forma de inteiro 1, 0 ou -1
+
+    :param peca: peca com qualquer representacao
+    :return: inteiro correspondente a peca
+    """
     return 1 if peca_para_str(peca) == '[X]' else (-1 if peca_para_str(peca) == '[O]' else 0)
 
 
 def inteiro_para_peca(inteiro):
+    """ Funcao auxiliar que converte um inteiro valido na sua peca correspondente
+
+    :param inteiro: (int) inteiro 1, -1 ou 0
+    :return: peca correspondente de acordo com a associacao feita na funcao anterior
+    """
     dic_pecas = {1: cria_peca('X'), -1: cria_peca('O'), 0: cria_peca(' ')}
     return dic_pecas[inteiro]
 
+#  2.1.3 TAD tabuleiro
+
 
 def cria_tabuleiro():
+    """
+
+    :return:
+    """
     return [[cria_peca(' '), cria_peca(' '), cria_peca(' ')], [cria_peca(' '), cria_peca(' '), cria_peca(' ')],
             [cria_peca(' '), cria_peca(' '), cria_peca(' ')]]
 
 
 def cria_copia_tabuleiro(tab):
     return [linha.copy() for linha in tab]
+
+
+def pos_coordenadas(pos):
+    """ Funcao auxiliar de obtencao de coordenadas(index da posicao na linha e index da linha de uma posicao num tuplo
+         ou lista) de um posicao especificada
+
+    :param pos: (list) posicao com a representacao escolhida neste caso uma lista
+    :return: (tuple) coordenadas da posicao no tabuleiro
+    """
+    pos_dic = {'a1': (0, 0), 'b1': (1, 0), 'c1': (2, 0), 'a2': (0, 1), 'b2': (1, 1), 'c2': (2, 1), 'a3': (0, 2),
+               'b3': (1, 2), 'c3': (2, 2)}
+    return pos_dic[posicao_para_str(pos)]
 
 
 def obter_peca(tab, pos):
@@ -148,10 +233,10 @@ def move_peca(tab, p1, p2):
 
 
 def jogadores_ganhadores(tab):
-    dic_ganhador = {-1: (cria_peca('O'), cria_peca('O'), cria_peca('O')),
-                    1: (cria_peca('X'), cria_peca('X'), cria_peca('X'))}
-    return [key for key in dic_ganhador for i in range(1, 4) if obter_vetor(tab, str(i)) == dic_ganhador[key]
-            or obter_vetor(tab, chr(ord('a') + (i - 1))) == dic_ganhador[key]]
+    dic_ganhador = ((cria_peca('O'), cria_peca('O'), cria_peca('O')),
+                    (cria_peca('X'), cria_peca('X'), cria_peca('X')))
+    return [win for win in dic_ganhador for i in range(1, 4) if obter_vetor(tab, str(i)) == win
+            or obter_vetor(tab, chr(ord('a') + (i - 1))) == win]
 
 
 def n_pecas_jog(tab, jog):
@@ -188,8 +273,8 @@ def eh_tabuleiro(tab):
             if type(valor) != int or valor not in valores_posiveis:
                 return type(valor) == int and valor in valores_posiveis
     n_pecas = [n_pecas_jog(tab, jog) for jog in valores_posiveis[:2]]
-    dif_pecas = [abs(len([j for i in tab for j in i if j == cria_peca('X')]) - len(
-        [j for i in tab for j in i if j == cria_peca('O')])) <= 1]
+    dif_pecas = [abs(len([j for i in tab for j in i if pecas_iguais(j, cria_peca('X'))]) - len(
+        [j for i in tab for j in i if pecas_iguais(j, cria_peca('O'))])) <= 1]
     res = [len(jogadores_ganhadores(tab)) <= 1] + dif_pecas + [i <= 3 for i in n_pecas]
     return False not in res
 
@@ -217,11 +302,10 @@ def tuplo_para_tabuleiro(tup):
 
 
 def obter_ganhador(tab):
-    dic_ganhador = {cria_peca('O'): (cria_peca('O'), cria_peca('O'), cria_peca('O')),
-                    cria_peca('X'): (cria_peca('X'), cria_peca('X'), cria_peca('X'))}
-    res = [key for key in dic_ganhador for i in range(1, 4) if obter_vetor(tab, str(i)) == dic_ganhador[key]
-           or obter_vetor(tab, chr(ord('a') + (i - 1))) == dic_ganhador[key]]
-    return res[0] if res else cria_peca(' ')
+    ganhador = ((cria_peca('O'), cria_peca('O'), cria_peca('O')), (cria_peca('X'), cria_peca('X'), cria_peca('X')))
+    res = [win for win in ganhador for i in range(1, 4) if obter_vetor(tab, str(i)) == win
+           or obter_vetor(tab, chr(ord('a') + (i - 1))) == win]
+    return res[0][0] if res else cria_peca(' ')
 
 
 def obter_posicoes_livres(tab):
@@ -250,9 +334,9 @@ def obter_movimento_manual(tab, jog):
     pos_possiveis = [posicao_para_str(cria_posicao(c, l)) for l in valores_posiveis_l for c in valores_posiveis_c]
     if n_pecas_jog(tab, jog) < 3:
         pos = input('Turno do jogador. Escolha uma posicao: ')
-        if pos not in pos_possiveis or not eh_posicao_livre(tab, pos):
+        if pos not in pos_possiveis or not eh_posicao_livre(tab, cria_posicao(pos[0], pos[1])):
             raise ValueError('obter_movimento_manual: escolha invalida')
-        return cria_posicao(obter_pos_c(pos), obter_pos_l(pos)),
+        return cria_posicao(pos[0], pos[1]),
     mov = input('Turno do jogador. Escolha um movimento: ')
     if len(mov) < 4 or mov[:2] not in pos_possiveis or mov[2:] not in pos_possiveis:
         raise ValueError('obter_movimento_manual: escolha invalida')
@@ -263,8 +347,7 @@ def obter_movimento_manual(tab, jog):
             mov_2 == mov_1 and not block(tab, jog)) or (pecas_iguais(obter_peca(tab, mov_2), jog) and not
     posicoes_iguais(mov_1, mov_2)):
         raise ValueError('obter_movimento_manual: escolha invalida')
-    return cria_posicao(obter_pos_c(mov[:2]), obter_pos_l(mov[:2])), cria_posicao(obter_pos_c(mov[2:]),
-                                                                                  obter_pos_l(mov[2:]))
+    return cria_posicao(mov[0], mov[1]), cria_posicao(mov[2], mov[3])
 
 
 def auto_coloc(tab, jog):
@@ -314,7 +397,7 @@ def obter_movimento_auto(tab, jog, dif):
         return auto_coloc(tab, jog),
     if block(tab, jog):
         return obter_posicoes_jogador(tab, jog)[0], obter_posicoes_jogador(tab, jog)[0]
-    if dif == 'facil' or (dif == 'normal' and minimax(tab, jog, 1, ())[0] == peca_para_inteiro(cria_peca(' '))):
+    if dif == 'facil' or (dif == 'normal' and pecas_iguais(minimax(tab, jog, 1, ())[0], cria_peca(' '))):
         for pos in obter_posicoes_jogador(tab, jog):
             for pos_adj in obter_posicoes_adjacentes(pos):
                 if eh_posicao_livre(tab, pos_adj):
@@ -326,52 +409,28 @@ def obter_movimento_auto(tab, jog, dif):
 
 
 def moinho(jog, dif):
-    dic_peca = {'[X]': cria_peca('X'), '[O]': cria_peca('O')}
-    niveis = ('facil', 'normal', 'dificil')
-    if type(jog) != str or type(dif) != str or jog not in dic_peca or dif not in niveis:
+    if jog not in ('[X]', '[O]') or dif not in ('facil', 'normal', 'dificil'):
         raise ValueError('moinho: argumentos invalidos')
-    tab = cria_tabuleiro()
-    jog = dic_peca[jog]
-    res = cria_peca(' ')
+    tab, res = cria_tabuleiro(), cria_peca(' ')
+    jog = cria_peca('X') if jog == '[X]' else cria_peca('O')
     adv = inteiro_para_peca(-peca_para_inteiro(jog))
+    turno = -1 if pecas_iguais(jog, cria_peca('O')) else 1
     print('Bem-vindo ao JOGO DO MOINHO. Nivel de dificuldade {}.'.format(dif))
     print(tabuleiro_para_str(tab))
-    if pecas_iguais(jog, cria_peca('O')):
-        while pecas_iguais(res, cria_peca(' ')):
+    while pecas_iguais(res, cria_peca(' ')):
+        if turno == 1:
+            mov = obter_movimento_manual(tab, jog)
+            tab = coloca_peca(tab, jog, mov[0]) if len(obter_posicoes_jogador(tab, jog)) \
+                                                                               < 3 else move_peca(tab, mov[0], mov[1])
+            res = obter_ganhador(tab)
+            print(tabuleiro_para_str(tab))
+            turno *= -1
+        else:
             print('Turno do computador ({}):'.format(dif))
-            if len(obter_posicoes_jogador(tab, adv)) < 3:
-                tab = coloca_peca(tab, adv, obter_movimento_auto(tab, adv, dif)[0])
-            else:
-                mov = obter_movimento_auto(tab, adv, dif)
-                tab = move_peca(tab, mov[0], mov[1])
-            res = obter_ganhador(tab)
-            print(tabuleiro_para_str(tab))
-            if not pecas_iguais(res, cria_peca(' ')):
-                break
-            if len(obter_posicoes_jogador(tab, jog)) < 3:
-                tab = coloca_peca(tab, jog, obter_movimento_manual(tab, jog)[0])
-            else:
-                mov = obter_movimento_manual(tab, jog)
-                tab = move_peca(tab, mov[0], mov[1])
-            res = obter_ganhador(tab)
-            print(tabuleiro_para_str(tab))
-    else:
-        while pecas_iguais(res, cria_peca(' ')):
-            if len(obter_posicoes_jogador(tab, adv)) < 3:
-                tab = coloca_peca(tab, jog, obter_movimento_manual(tab, jog)[0])
-            else:
-                mov = obter_movimento_manual(tab, jog)
-                tab = move_peca(tab, mov[0], mov[1])
-            res = obter_ganhador(tab)
-            print(tabuleiro_para_str(tab))
-            if not pecas_iguais(res, cria_peca(' ')):
-                break
-            print('Turno do computador ({}):'.format(dif))
-            if len(obter_posicoes_jogador(tab, adv)) < 3:
-                tab = coloca_peca(tab, adv, obter_movimento_auto(tab, adv, dif)[0])
-            else:
-                mov = obter_movimento_auto(tab, adv, dif)
-                tab = move_peca(tab, mov[0], mov[1])
+            mov = obter_movimento_auto(tab, adv, dif)
+            tab = coloca_peca(tab, adv, mov[0]) if len(obter_posicoes_jogador(tab, adv)) < 3 else move_peca(tab, mov[0],
+                                                                                                            mov[1])
+            turno *= -1
             res = obter_ganhador(tab)
             print(tabuleiro_para_str(tab))
     return peca_para_str(res)
